f0f995a13c1ebeee54ff932fe07e4bbdead008fa
Anja Siewert <anja.siewert@live.de>
2023-07-20 12:33:33 +0200

    add display function

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-const count = 5;
+// const imageContainer = document.getElementById('image-container');
+// const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+const count = 30;
 const apiKey = 'BvlJDIiVQBrJpwHa8RlTm7Xf_tFiPHKKXirJbkojUs0';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create Elements For Links & Photos, Add to DOM
+// Helper function
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            targent: '_blank',
+        });
+    
+        const image = document.createElement('img');
+        setAttributes(item, {
+            src: photosArray.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
+
+        item.appendChild(image);
+        image.appendChild(item);
+    })
+};
 
 async function getPhotos() {
     try {
         const response = await fetch(apiURL);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
-        // Catch error here
+        alert(error);
     }
 };
 
